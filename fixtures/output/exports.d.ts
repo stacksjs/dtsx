6786b1403c130ac7daa0c7c96650272ccb556f20
Chris <chrisbreuer93@gmail.com>
2024-11-04 17:32:21 +0100

    chore: wip

---
+++ fixtures/output/exports.d.ts
@@ -0,0 +1,8 @@
+import { generate } from './generate';
+export { generate, dtsConfig }
+
+export { config } from './config'
+export * from './extract'
+export * from './generate'
+export * from './types'
+export * from './utils'
