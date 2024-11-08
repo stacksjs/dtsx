0d85166d961b99f11ff496dbe30cd833ec58a563
Chris <chrisbreuer93@gmail.com>
2024-11-08 16:47:32 +0100

    chore: update `bun-plugin-dtsx`

--- build.ts
+++ build.ts
@@ -1,6 +1,5 @@
 import { log } from '@stacksjs/cli'
 import { fs } from '@stacksjs/storage'
-// @ts-expect-error - types are missing for now
 import { dts } from 'bun-plugin-dtsx'
 
 log.info('Building...')
