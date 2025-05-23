1cb6e3fa6bd17f045c10a461366ec01db4205ef4
Chris <chrisbreuer93@gmail.com>
2025-05-23 11:27:07 -0700

    chore: add `defaultConfig` export
    chore: wip

--- src/config.ts
+++ src/config.ts
@@ -2,19 +2,21 @@ import type { DtsGenerationConfig } from './types'
 import process from 'node:process'
 import { loadConfig } from 'bunfig'
 
+export const defaultConfig: DtsGenerationConfig = {
+  cwd: process.cwd(),
+  root: './src',
+  entrypoints: ['**/*.ts'],
+  outdir: './dist',
+  keepComments: true,
+  clean: true,
+  tsconfigPath: './tsconfig.json',
+  outputStructure: 'mirror',
+  verbose: false,
+}
+
 // Get loaded config
-// @ts-expect-error bunfig error due to current dtsx incompatibility (0009)
 // eslint-disable-next-line antfu/no-top-level-await
 export const config: DtsGenerationConfig = await loadConfig({
   name: 'dts',
-  defaultConfig: {
-    cwd: process.cwd(),
-    root: './src',
-    entrypoints: ['**/*.ts'],
-    outdir: './dist',
-    keepComments: true,
-    clean: true,
-    tsconfigPath: './tsconfig.json',
-    outputStructure: 'mirror',
-  },
+  defaultConfig,
 })
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { config } from './config'
+export * from './config'
 export * from './extract'
 export * from './generate'
 export * from './types'
