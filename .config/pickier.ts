6a47af8f12d4b1aadeea32b7487e7f22258459b6
glennmichael123 <gtorregosa@gmail.com>
2026-02-16 18:41:46 +0800

    chore: wip

--- .config/pickier.ts
+++ .config/pickier.ts
@@ -2,7 +2,7 @@ import type { PickierConfig } from 'pickier'
 
 const config: PickierConfig = {
   verbose: false,
-  ignores: ['**/node_modules/**', '**/dist/**', '**/build/**', '**/.test-cli/**', '**/test/fixtures/**', '**/profiler.ts', '**/profiler-deep.ts'],
+  ignores: ['**/node_modules/**', '**/dist/**', '**/build/**', '**/.test-cli/**', '**/test/fixtures/**', '**/profiler.ts', '**/profiler-deep.ts', '**/CHANGELOG.md'],
 
   lint: {
     extensions: ['ts', 'js'],
