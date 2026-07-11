298bb649f6946204395e0eb6e6900fd389f6272e
Chris <chrisbreuer93@gmail.com>
2026-07-11 12:22:54 -0700

    fix(zig): remove unstable run arguments API

--- packages/zig-dtsx/build.zig
+++ packages/zig-dtsx/build.zig
@@ -54,7 +54,6 @@ pub fn build(b: *std.Build) void {
     // Run step
     const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
-    if (b.args) |args| run_cmd.addArgs(args);
     const run_step = b.step("run", "Run the CLI");
     run_step.dependOn(&run_cmd.step);
 
